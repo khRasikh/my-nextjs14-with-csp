cb5d92e23d636b9d08d762abc13d2533986357f7
khRasikh <kh.rasikh542@gmail.com>
2024-02-06 17:59:23 +0100

    adding CSP

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+      return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value:
+                  "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self' *.somewhere.com; script-src 'self'",
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+              {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+              },
+              {
+                key: 'Referrer-Policy',
+                value: 'origin-when-cross-origin',
+              },
+              {
+                key: 'Permissions-Policy',
+                value: "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
+              },
+            ],
+          },
+        ];
+    },
+  };
+  
 
 export default nextConfig;
