bf8a46a38d56d7616b4661cc711406d93325dc2a
khRasikh <kh.rasikh542@gmail.com>
2024-02-07 15:26:52 +0100

    adding ssr csp

--- next.config.mjs
+++ next.config.mjs
@@ -6,35 +6,35 @@ const CSPHeaders = `default-src *; script-src 'unsafe-inline' 'unsafe-eval' 'sel
                 form-action 'self';`
 
 const nextConfig = {
-    async headers() {
-      return [
-          {
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'Content-Security-Policy',
-                value: CSPHeaders
-              },
-              {
-                key: 'X-Frame-Options',
-                value: 'DENY',
-              },
-              {
-                key: 'X-Content-Type-Options',
-                value: 'nosniff',
-              },
-              {
-                key: 'Referrer-Policy',
-                value: 'origin-when-cross-origin',
-              },
-              {
-                key: 'Permissions-Policy',
-                value: "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
-              },
-            ],
-          },
-        ];
-    },
+    // async headers() {
+    //   return [
+    //       {
+    //         source: '/(.*)',
+    //         headers: [
+    //           {
+    //             key: 'Content-Security-Policy',
+    //             value: CSPHeaders
+    //           },
+    //           {
+    //             key: 'X-Frame-Options',
+    //             value: 'DENY',
+    //           },
+    //           {
+    //             key: 'X-Content-Type-Options',
+    //             value: 'nosniff',
+    //           },
+    //           {
+    //             key: 'Referrer-Policy',
+    //             value: 'origin-when-cross-origin',
+    //           },
+    //           {
+    //             key: 'Permissions-Policy',
+    //             value: "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
+    //           },
+    //         ],
+    //       },
+    //     ];
+    // },
   };
   
 
