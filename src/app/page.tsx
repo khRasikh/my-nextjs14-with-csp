cd0648e26895dd949781ed2713f9639015661b16
khRasikh <kh.rasikh542@gmail.com>
2024-02-07 17:39:55 +0100

    test x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,30 @@ export default function Home() {
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          CSP Report Only Example
+          Full CSP{' '}
+          <small>
+            (<code>content-security-policy</code>)
+          </small>
         </h2>
-        <p>
-          Nonce: <code>{headersList.get('x-nonce') ?? 'Not found'}</code>
-        </p>
+
+        <dl>
+          <dt>
+            <code className="font-bold">x-nonce</code> header:
+          </dt>
+          <dd className="mb-8">
+            <code>{headersList.get('x-nonce') ?? 'Not found'}</code>
+          </dd>
+          <dt>
+            <code className="font-bold">content-security-policy</code> header:
+          </dt>
+          <dd className="mb-8">
+            <pre className="whitespace-pre-wrap">
+              <code>{headersList.get('content-security-policy')}</code>
+            </pre>
+          </dd>
+        </dl>
       </div>
+
     </div>
   );
 }
