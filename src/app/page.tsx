d231c1e6ac207cc6671137536f89691b2b208515
khRasikh <kh.rasikh542@gmail.com>
2024-02-08 10:41:49 +0100

    reversing back again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ import Script from "next/script";
 export default function Page() {
   const nonce = headers().get("x-nonce") ?? "";
 
-  return <Script src="https://..." strategy="afterInteractive" nonce={nonce} />;
+  return <Script src="https://my-nextjs14-with-csp.vercel.app/" strategy="afterInteractive" nonce={nonce} />;
 }
