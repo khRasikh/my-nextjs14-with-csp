2852dcc90e1edcc0a8f70734fa76d2de6a445b66
khRasikh <kh.rasikh542@gmail.com>
2024-02-08 15:45:12 +0100

    updating middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,66 @@
-import { headers } from "next/headers";
-import Script from "next/script";
 
-export default function Page() {
-  const nonce = headers().get("x-nonce") ?? "";
+import { headers } from 'next/headers';
 
-  return <Script src="https://my-nextjs14-with-csp.vercel.app/" strategy="afterInteractive" nonce={nonce} />;
+export default function Home() {
+  const headersList = headers();
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <small>
+            <h3>Congratulations! You're setup with CSP.</h3>
+            <h2>Here are the CSP roles:</h2>
+          </small>
+        </h2>
+
+        <dl>
+          <dt>
+            <code className="font-bold">x-nonce</code> header:
+          </dt>
+          <dd className="mb-8">
+            <code>{headersList.get('x-nonce') || 'Not found'}</code>
+          </dd>
+          <dt>
+            <code className="font-bold">content-security-policy</code>:
+          </dt>
+          <dd className="mb-8">
+            <pre className="whitespace-pre-wrap">
+              <code>{headersList.get('content-security-policy')}</code>
+            </pre>
+          </dd>
+          
+          <dt>
+            <code className="font-bold">Permissions-Policy</code>:
+          </dt>
+          <dd className="mb-8">
+            <pre className="whitespace-pre-wrap">
+              <code>{headersList.get('Permissions-Policy')}</code>
+            </pre>
+          </dd>
+          
+
+          <dt>
+            <code className="font-bold">X-Content-Type-Options</code>:
+          </dt>
+          <dd className="mb-8">
+            <pre className="whitespace-pre-wrap">
+              <code>{headersList.get('X-Content-Type-Options')}</code>
+            </pre>
+          </dd>
+          
+
+          <dt>
+            <code className="font-bold">Referrer-Policy</code>:
+          </dt>
+          <dd className="mb-8">
+            <pre className="whitespace-pre-wrap">
+              <code>{headersList.get('Referrer-Policy')}</code>
+            </pre>
+          </dd>
+          
+        </dl>
+      </div>
+
+    </div>
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,63 +1,66 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+  // Skip all paths that should not be internationalized
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
+
+// CSP Implementation
 export function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
-  console.log("test nonce middleware", nonce)
-  const cspHeader = `
+  // this is needed because when passing throught this middleware the microsoft auth does not redirect properly and throws lot of errors
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+
+  const CSPDirectivesForRequestResponse = [
+    { key: "X-Content-Type-Options", value: "nosniff" },
+    { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+    { key: "Permissions-Policy", value: "geolocation=(self)" },
+    { key: "X-Frame-Options", value: "SAMEORIGIN" },
+    { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+    { key: "X-DNS-Prefetch-Control", value: "on" },
+  ];
+
+  const DirectivesForCSP = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://alcdn.msauth.net;
+    style-src 'self' 'nonce-${nonce}' https://fonts.googleapis.com;
     img-src 'self' blob: data:;
-    font-src 'self';
+    font-src 'self' https://fonts.googleapis.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-  `
+    connect-src 'self' https://login.microsoftonline.com https://graph.microsoft.com/v1.0/me;
+    report-uri /api/csp;
+    frame-src 'self';
+    worker-src 'self';
+    sandbox allow-forms allow-same-origin allow-scripts;
+  `;
   // Replace newline characters and spaces
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim()
+  const CSPHeades = DirectivesForCSP.replace(/\s{2,}/g, " ").trim();
 
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
-  requestHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue)
-  requestHeaders.set('X-Content-Type-Options', 'nosniff')
-  requestHeaders.set('Referrer-Policy', 'strict-origin-when-cross-origin')
-  requestHeaders.set('Permissions-Policy', 'geolocation=(self)')
+  const requestHeaders = new Headers(request.headers);
+
+  let CSPReportOnlyActivation = process.env.NEXT_PUBLIC_CSP_REPORT_ONLY_ACTIVATION ?? true; //default = true
+  let CSPHeadersName = CSPReportOnlyActivation ? "Content-Security-Policy" : "Content-Security-Policy-Report-Only";
+
+  // Request Headers CSP
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set(CSPHeadersName, CSPHeades);
+  CSPDirectivesForRequestResponse.forEach(({ key, value }) => {
+    requestHeaders.set(key, value);
+  });
 
   const response = NextResponse.next({
     request: {
       headers: requestHeaders,
     },
-  })
+  });
 
-  // Adding headers to response
-  response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue)
-  response.headers.set('X-Content-Type-Options', 'nosniff')
-  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
-  response.headers.set('Permissions-Policy', 'geolocation=(self)')
+  // Response Headers CSP
+  response.headers.set(CSPHeadersName, CSPHeades);
+  CSPDirectivesForRequestResponse.forEach(({ key, value }) => {
+    response.headers.set(key, value);
+  });
 
-  return response
-}
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
+  return response;
 }
